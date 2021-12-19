2f6c23774ee000f11c8e91b3630acff26ed5af35
exit <exit>
2021-12-19 16:50:19 +0400

    add the schema

--- db/migrate/20211219060000_create_product_variants.rb
+++ db/migrate/20211219060000_create_product_variants.rb
@@ -2,7 +2,7 @@ class CreateProductVariants < ActiveRecord::Migration[6.1]
   def change
     create_table :product_variants do |t|
       t.string :title, null:false
-      t.decimal :price
+      t.decimal :price, null:false, precision:15, price:2
 
       t.timestamps
     end
