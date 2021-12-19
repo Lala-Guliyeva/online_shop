46deba97470e272efa8fa9d33cf4aae530128fa5
exit <exit>
2021-12-19 14:57:44 +0400

    Models, associations created

--- db/migrate/20211219053914_create_products.rb
+++ db/migrate/20211219053914_create_products.rb
@@ -1,7 +1,7 @@
 class CreateProducts < ActiveRecord::Migration[6.1]
   def change
     create_table :products do |t|
-      t.string :title
+      t.string :title, null:false
       t.text :description
       t.decimal :price, precision:15, scale:2
 
--- db/migrate/20211219060000_create_product_variants.rb
+++ db/migrate/20211219060000_create_product_variants.rb
@@ -1,7 +1,7 @@
 class CreateProductVariants < ActiveRecord::Migration[6.1]
   def change
     create_table :product_variants do |t|
-      t.string :title
+      t.string :title, null:false
       t.decimal :price
 
       t.timestamps
